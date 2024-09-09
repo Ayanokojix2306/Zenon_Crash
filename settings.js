5ce74a642c9248d9969320f5d7c5918da226cc1d
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-09-09 08:59:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.wm = "Toxxic Tech."
 global.link = 'https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U'
 
 //custom prefix
-global.prefa = ['','!','.','#','&']
+global.prefa = ['-']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
